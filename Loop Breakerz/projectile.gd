7a839dcd47018cc9cec02427e9b162b5c87196e7
Kosina Zoltán <kosinaz@gmail.com>
2024-12-18 20:02:30 +0100

    add projectiles

--- Loop Breakerz/projectile.gd
+++ Loop Breakerz/projectile.gd
@@ -4,6 +4,9 @@ extends KinematicBody2D
 export var speed = 200
 var direction = Vector2()
 
+# Explosion scene reference
+export (PackedScene) var explosion_scene
+
 # Set the direction of the projectile
 func set_direction(dir: Vector2):
 	direction = dir.normalized()
@@ -14,4 +17,16 @@ func _physics_process(delta):
 	
 	# Destroy the projectile if it collides with the map
 	if collision:
-		queue_free()
+		_trigger_explosion()
+		queue_free()  # Destroy the projectile after explosion
+
+# Function to trigger the explosion
+func _trigger_explosion():
+	# Instance the explosion scene
+	var explosion = explosion_scene.instance()
+
+	# Set the explosion position to the projectile's current position
+	explosion.global_position = global_position
+
+	# Add the explosion to the scene (assuming map node or parent is available)
+	get_parent().add_child(explosion)
