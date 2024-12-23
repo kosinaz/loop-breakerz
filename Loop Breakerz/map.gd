f7174cd31a724954aee029eb4271d3ceb905f7ad
Kosina Zoltán <kosinaz@gmail.com>
2024-12-23 20:59:24 +0100

    refine rooms

--- Loop Breakerz/map.gd
+++ Loop Breakerz/map.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
 # Room and corridor parameters
-export (int) var room_min_size = 5
-export (int) var room_max_size = 10
-export (int) var corridor_width = 3
+export (int) var room_min_size = 7
+export (int) var room_max_size = 15
+export (int) var corridor_min_length = 5
+export (int) var corridor_max_length = 9
 export (int) var tile_id = 0  # ID for walkable tiles
 
 onready var tilemap = $TileMap
@@ -14,16 +15,14 @@ func _ready():
 	rng.seed = OS.get_ticks_usec()
 
 	# Generate Room A at the origin
-	var room_a = generate_room(Vector2.ZERO)
+	generate_room(Vector2.ZERO)
+	
+func _process(_delta):
+	if Input.is_action_just_released("ui_accept"):
+		# warning-ignore:return_value_discarded
+		get_tree().reload_current_scene()
 
-	# Generate Room B at least 10 tiles away
-	var room_b_position = Vector2(15, 0)
-	var room_b = generate_room(room_b_position)
-
-	# Connect Room A and Room B with a corridor
-	connect_rooms(room_a, room_b)
-
-func generate_room(origin: Vector2) -> Rect2:
+func generate_room(origin: Vector2) -> Dictionary:
 	# Randomly determine room size
 	var width = rng.randi_range(room_min_size, room_max_size)
 	var height = rng.randi_range(room_min_size, room_max_size)
@@ -32,34 +31,58 @@ func generate_room(origin: Vector2) -> Rect2:
 	for x in range(width):
 		for y in range(height):
 			tilemap.set_cellv(origin + Vector2(x, y), tile_id)
+	
+	var doors = []
+	
+	# Top door
+	if randi() % 2:
+		var door_position = origin + Vector2(randi() % (width - 4) + 2, -1)
+		for x in range(-1, 2):
+			tilemap.set_cellv(door_position + Vector2(x, 0), tile_id)
+		doors.append({
+			"position": door_position,
+			"direction": "up",
+		})
+	
+	# Bottom door
+	if randi() % 2:
+		var door_position = origin + Vector2(randi() % (width - 4) + 2, height)
+		for x in range(-1, 2):
+			tilemap.set_cellv(door_position + Vector2(x, 0), tile_id)
+		doors.append({
+			"position": door_position,
+			"direction": "down",
+		})
+			
+	# Left door
+	if randi() % 2:
+		var door_position = origin + Vector2(-1, randi() % (height - 4) + 2)
+		for y in range(-1, 2):
+			tilemap.set_cellv(door_position + Vector2(0, y), tile_id)
+		doors.append({
+			"position": door_position,
+			"direction": "left",
+		})
+	
+	# Right door
+	if randi() % 2 :
+		var door_position = origin + Vector2(width, randi() % (height - 4) + 2)
+		for y in range(-1, 2):
+			tilemap.set_cellv(door_position + Vector2(0, y), tile_id)
+		doors.append({
+			"position": door_position,
+			"direction": "right",
+		})
 
 	# Update the autotile bitmask for the entire room region
 	tilemap.update_bitmask_region(origin, Vector2(width, height))
 
 	# Return the room's bounds for corridor placement
-	return Rect2(origin, Vector2(width, height))
-
-func connect_rooms(room_a: Rect2, room_b: Rect2):
-	# Find the center of each room
-	var center_a = room_a.position + room_a.size / 2
-
-	# Determine valid entrance tiles for both rooms
-	var entrance_a = get_valid_entrance(room_a)
-	var entrance_b = get_valid_entrance(room_b)
-
-	# Create a horizontal corridor
-	var start_x = int(min(entrance_a.x, entrance_b.x))
-	var end_x = int(max(entrance_a.x, entrance_b.x))
-
-	for x in range(start_x, end_x + 1):
-		for y in range(corridor_width):
-			var y_pos = int(center_a.y + y - corridor_width / 2)
-			tilemap.set_cellv(Vector2(x, y_pos), tile_id)
-
-	# Update the autotile bitmask for the corridor region
-	var corridor_region_origin = Vector2(start_x, center_a.y - int(corridor_width / 2))
-	var corridor_region = Vector2(end_x - start_x + 1, corridor_width)
-	tilemap.update_bitmask_region(corridor_region_origin, corridor_region)
+	return {
+		"origin": origin, 
+		"size": Vector2(width, height),
+		"doors": doors,
+	}
 
 func get_valid_entrance(room: Rect2) -> Vector2:
 	# Get valid tiles for corridor entrances, excluding corners
