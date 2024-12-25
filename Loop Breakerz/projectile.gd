34b7ea157ad8d21ce927fb3ade809455084a6098
Kosina Zoltán <kosinaz@gmail.com>
2024-12-25 16:23:05 +0100

    destroy iterator

--- Loop Breakerz/projectile.gd
+++ Loop Breakerz/projectile.gd
@@ -17,16 +17,20 @@ func _physics_process(delta):
 	
 	# Destroy the projectile if it collides with the map
 	if collision:
-		_trigger_explosion()
+		handle_collision(collision)
 		queue_free()  # Destroy the projectile after explosion
 
 # Function to trigger the explosion
-func _trigger_explosion():
+func handle_collision(collision):
 	# Instance the explosion scene
 	var explosion = explosion_scene.instance()
 
 	# Set the explosion position to the projectile's current position
 	explosion.global_position = global_position
-
+	
+	var collider = collision.collider
+	if collider.has_method("take_damage"):
+		collider.take_damage(1)
+	
 	# Add the explosion to the scene (assuming map node or parent is available)
 	get_parent().add_child(explosion)
